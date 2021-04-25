2362e953496d05098987f0b8b0c73599e1ac19da
Zach Cossman <Zachcos@gmail.com>
2021-04-25 18:03:15 -0400

    Wire up mobile menu toggle functionality

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,21 @@ accItems.forEach((item) => {
 
   })
 })
+
+const btn = document.querySelector('.nav__mobileBtn');
+const hamIcon = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.close');
+const menu = document.querySelector('.navContainer')
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('menuOpen')
+  if (btn.classList.contains('menuOpen')) {
+    hamIcon.classList.remove('show');
+    closeIcon.classList.add('show');
+    menu.classList.add('menuOpen');
+  } else {
+    hamIcon.classList.add('show');
+    closeIcon.classList.remove('show');
+    menu.classList.remove('menuOpen');
+  }
+})
